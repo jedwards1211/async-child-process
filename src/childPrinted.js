d65a9a4df2a5088a532d3e29a33d83f551e45f75
Andy Edwards <jedwards@fastmail.com>
2016-10-24 19:54:55 -0500

    change childOutput API to childPrinted and with stream name as last argument

--- src/childPrinted.js
+++ src/childPrinted.js
@@ -3,15 +3,21 @@
 import type {ChildProcess} from 'child_process'
 import type {Readable} from 'stream'
 
-export default function childOutput(
+export default function childPrinted(
   child: ChildProcess,
-  stream: Readable,
-  predicate: (output: string) => boolean | RegExp
+  predicate: (output: string) => boolean | RegExp,
+  stream?: 'stdout' | 'stderr'
 ): Promise<string> {
+  const streams: Array<Readable> = stream === 'stdout'
+    ? [child.stdout]
+    : stream === 'stderr'
+      ? [child.stderr]
+      : [child.stdout, child.stderr]
+
   return new Promise((_resolve: (data: string) => void, _reject: (error: Error) => void) => {
     function unlisten() {
       child.removeAllListeners()
-      stream.removeListener('data', onData)
+      streams.forEach(stream => stream.removeListener('data', onData))
     }
 
     function resolve(data: string) {
@@ -28,7 +34,7 @@ export default function childOutput(
       const message = data.toString()
       if (predicate instanceof RegExp ? predicate.test(message) : predicate(message)) resolve(data.toString())
     }
-    stream.on('data', onData)
+    streams.forEach(stream => stream.on('data', onData))
 
     const onClose = () => reject(new Error('stream closed'))
     const onExit = () => reject(new Error('process exited'))
--- test/childPrintedTests.js
+++ test/childPrintedTests.js
@@ -1,21 +1,21 @@
-import childStdout from '../src/childStdout'
+import childPrinted from '../src/childPrinted'
 import {expect} from 'chai'
 import {spawn} from 'child_process'
 
-describe('childStdout', () => {
+describe('childPrinted', () => {
   it('resolves to output matching regexp', async () => {
     const child = spawn('node', ['-e', `
     setTimeout(function () { console.log('testa') }, 25)
     setTimeout(function () { console.log('test2') }, 50)
     `])
-    expect((await childStdout(child, /test\d/)).trim()).to.equal('test2')
+    expect((await childPrinted(child, /test\d/)).trim()).to.equal('test2')
   })
   it('resolves to output matching predicate', async () => {
     const child = spawn('node', ['-e', `
     setTimeout(function () { console.log('testa') }, 25)
     setTimeout(function () { console.log('test2') }, 50)
     `])
-    expect((await childStdout(child, output => output.trim() === 'test2')).trim()).to.equal('test2')
+    expect((await childPrinted(child, output => output.trim() === 'test2')).trim()).to.equal('test2')
   })
   it('rejects if child exits before output', async () => {
     const child = spawn('node', ['-e', `
@@ -24,7 +24,7 @@ describe('childStdout', () => {
     `])
     let error
     try {
-      await childStdout(child, /test2/)
+      await childPrinted(child, /test2/)
     } catch (e) {
       error = e
     }
@@ -35,10 +35,32 @@ describe('childStdout', () => {
     setTimeout(function () { console.log('test2') }, 500)
     `])
     let error
-    let promise = childStdout(child, /test2/).catch(e => error = e)
+    let promise = childPrinted(child, /test2/).catch(e => error = e)
     // not sure how to close the streams without killing the process, so I'll just fake it...
     child.emit('close')
     await promise
     expect(error.message).to.match(/stream closed/)
+  })
+  it('listens to both stdout and stderr by default', async () => {
+    const child = spawn('node', ['-e', `
+    console.log('test1')
+    console.error('test2')
+    `])
+    await childPrinted(child, /test1/)
+    await childPrinted(child, /test2/)
+  })
+  it('listens only to stdout if specified', async () => {
+    const child = spawn('node', ['-e', `
+    console.error('test1')
+    console.log('test2')
+    `])
+    expect(await childPrinted(child, /test\d/, 'stdout')).to.equal('test2\n')
+  })
+  it('listens only to stderr if specified', async () => {
+    const child = spawn('node', ['-e', `
+    console.log('test1')
+    console.error('test2')
+    `])
+    expect(await childPrinted(child, /test\d/, 'stderr')).to.equal('test2\n')
   })
 })
